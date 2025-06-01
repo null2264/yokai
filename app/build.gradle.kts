abbe60647353bb674ad4b7950a16c0501ccda763
Ahmad Ansori Palembani <palembani@gmail.com>
2025-06-01 18:05:35 +0700

    revert: "refactor: Replace Requery's SQLite with AndroidX's new KMP SQLite"
    This reverts commit f604e4e25682ab4e9fc8c1d96b34107d57b9db31.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -200,6 +200,7 @@ dependencies {
     implementation(libs.play.services.gcm)
 
     // Database
+    implementation(libs.sqlite.android)
     implementation(libs.bundles.sqlite)
 
     // Model View Presenter
