95514764edc7f0e15cb88aa01188a13e05fb9907
Ahmad Ansori Palembani <palembani@gmail.com>
2026-08-24 15:11:10 +0700

    chore: Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ val supportedAbis = setOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
 android {
     defaultConfig {
         applicationId = "eu.kanade.tachiyomi"
-        versionCode = 160
+        versionCode = 161
         versionName = _versionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
